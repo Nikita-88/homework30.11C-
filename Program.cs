f45c7ffcfc506d4b07750ecff59471abdb4a2e5b
Nikita-88 <151461265+Nikita-88@users.noreply.github.com>
2023-11-30 18:43:28 +0300

    Загрузил 3 задачи

--- Program.cs
+++ Program.cs
@@ -6,3 +6,22 @@ int [] array = {25, 2, 3, 64, 8, 5, 36, 4, 9, 53};
 for (int i=0; i <array.Length; i++)
     if (array[i]>20 && array [i]<90)
         Console.WriteLine($"[{i}]={array[i]}");
+// Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int [] array = {2,4,3,7,8,9,10,12,13,15};
+int count = 0;
+for (int i=0; i<array.Length; i++)
+    if (array[i] % 2 == 0)
+    count++;
+
+    Console.WriteLine(count);
+
+
+// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] array = {2.2, 5.1,6.3,4.2};
+double maxItem = array.Max(), minItem = array.Min();
+Console.WriteLine($"Max {maxItem}");
+Console.WriteLine($"Min {minItem}");
+Console.WriteLine(Math.Abs (maxItem -minItem));
+
